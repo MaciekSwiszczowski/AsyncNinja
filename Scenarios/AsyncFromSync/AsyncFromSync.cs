a9228b21f3fad886b59ba4235e4fbd3bade9c23f
Maciej_Swiszczowski <Maciej_Swiszczowski@edwards.com>
2024-02-11 16:35:56 +0100

    .NET upgrade, packages upgrade, small code refactoring

--- Scenarios/AsyncFromSync/AsyncFromSync.cs
+++ Scenarios/AsyncFromSync/AsyncFromSync.cs
@@ -1,57 +1,56 @@
 ﻿using System;
 using System.Threading.Tasks;
 
-namespace Scenarios.AsyncFromSync
+namespace Scenarios.AsyncFromSync;
+
+// more: https://github.com/StephenCleary/AsyncEx/wiki/AsyncContext
+// https://github.com/StephenCleary/Mvvm.Async
+
+public class AsyncFromSync : IRunnable
 {
-    // more: https://github.com/StephenCleary/AsyncEx/wiki/AsyncContext
-    // https://github.com/StephenCleary/Mvvm.Async
+    public string Title => "Async from sync";
+    public Order Order => Order.AsyncFromSync;
 
-    public class AsyncFromSync : IRunnable
+    public string Comment => "How to correctly (safely and without deadlocks) start an async method from a sync one?";
+
+    public async Task RunAsync()
     {
-        public string Title => "Async from sync";
-        public Order Order => Order.AsyncFromSync;
+        RunSync();
 
-        public string Comment => "How to correctly (safely and without deadlocks) start an async method from a sync one?";
+        Console.WriteLine("After RunSync().");
 
-        public async Task RunAsync()
-        {
-            RunSync();
-
-            Console.WriteLine("After RunSync().");
-
-            await Task.Delay(100);
+        await Task.Delay(100);
             
-            Console.WriteLine("After RunSync() and 100 ms delay.");
+        Console.WriteLine("After RunSync() and 100 ms delay.");
             
-            await Task.Delay(5000);
-        }
-
-        private static void RunSync()
-        {
-            Task.Run(async () =>
-            {
-                Console.WriteLine("Before the delay in RunSync().");
-                
-                await Task.Delay(200);
-
-                Console.WriteLine("After the 200 ms delay.");
-
-                try
-                {
-                    await MethodWithExceptionAsync();
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Exception from a different thread was caught");
-                }
-
-                // this exception will never be noticed
-                await Task.Run(() => throw new Exception());
-            });
-
-            Console.WriteLine("After Task.Run().");
-        }
-
-        private static Task MethodWithExceptionAsync() => Task.Run(() => throw new Exception());
+        await Task.Delay(5000);
     }
+
+    private static void RunSync()
+    {
+        var task = Task.Run(static async () =>
+        {
+            Console.WriteLine("Before the delay in RunSync().");
+                
+            await Task.Delay(200);
+
+            Console.WriteLine("After the 200 ms delay.");
+
+            try
+            {
+                await MethodWithExceptionAsync();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception from a different thread was caught");
+            }
+
+            // this exception will never be noticed
+            await Task.Run(static () => throw new Exception());
+        });
+
+        Console.WriteLine("After Task.Run().");
+    }
+
+    private static Task MethodWithExceptionAsync() => Task.Run(static () => throw new Exception());
 }
