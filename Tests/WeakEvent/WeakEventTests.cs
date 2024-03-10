f0642dfa2d78f7db26ecd0e78b4584faaa224f2c
Maciej_Swiszczowski <Maciej_Swiszczowski@edwards.com>
2024-03-10 22:43:47 +0100

    Code cleanup

--- Tests/WeakEvent/WeakEventTests.cs
+++ Tests/WeakEvent/WeakEventTests.cs
@@ -1,149 +1,164 @@
 using System;
+using System.Diagnostics;
 using AsyncAwaitBestPractices;
 using NUnit.Framework;
 using Shouldly;
 
-namespace Tests.WeakEvent
+namespace Tests.WeakEvent;
+
+public class WeakEventTests
 {
-    public class WeakEventTests
+    [Test]
+    public void WeakEventTest()
     {
-        [Test]
-        public void WeakEventTest()
+        WeakReference weakReference = null;
+        var longLiving = new LongLivingWithWeakEvent();
+
+        // Run this in a delegate so that the local variable gets garbage collected (if that's possible)
+        new Action(() =>
         {
-            WeakReference weakReference = null;
-            var longLiving = new LongLivingWithWeakEvent();
-
-            // Run this in a delegate so that the local variable gets garbage collected (if that's possible)
-            new Action(() =>
-            {
-                var sut = new ShortLiving(longLiving);
-                weakReference = new WeakReference(sut);
-            })();
+            var sut = new ShortLiving(longLiving);
+            weakReference = new WeakReference(sut);
+        })();
 
 
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-            GC.Collect();
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
 
-            weakReference.Target.ShouldBeNull();
-        }
+        weakReference.Target.ShouldBeNull();
+    }
 
-        [Test]
-        public void StaticWeakEventTest()
+    [Test]
+    public void StaticWeakEventTest()
+    {
+        WeakReference weakReference = null;
+
+        // Run this in a delegate so that the local variable gets garbage collected (if that's possible)
+        new Action(() =>
         {
-            WeakReference weakReference = null;
+            var sut = new ShortLiving();
+            sut.SubscribeToStaticWeakEvent();
 
-            // Run this in a delegate so that the local variable gets garbage collected (if that's possible)
-            new Action(() =>
-            {
-                var sut = new ShortLiving();
-                sut.SubscribeToStaticWeakEvent();
-
-                weakReference = new WeakReference(sut);
-            })();
+            weakReference = new WeakReference(sut);
+        })();
 
 
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-            GC.Collect();
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
 
-            weakReference.Target.ShouldBeNull();
-        }
+        weakReference.Target.ShouldBeNull();
+    }
 
-        [Test]
-        public void EventTest()
+    [Test]
+    public void EventTest()
+    {
+        WeakReference weakReference = null;
+        var longLiving = new LongLivingWithEvent();
+
+        // Run this in a delegate so that the local variable gets garbage collected (if that's possible)
+        new Action(() =>
         {
-            WeakReference weakReference = null;
-            var longLiving = new LongLivingWithEvent();
-
-            // Run this in a delegate so that the local variable gets garbage collected (if that's possible)
-            new Action(() =>
-            {
-                var sut = new ShortLiving(longLiving);
-                weakReference = new WeakReference(sut);
-            })();
+            var sut = new ShortLiving(longLiving);
+            weakReference = new WeakReference(sut);
+        })();
 
 
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-            GC.Collect();
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
 
-            weakReference.Target.ShouldNotBeNull();
-        }
+        weakReference.Target.ShouldNotBeNull();
+    }
 
-        [Test]
-        public void StaticEventTest()
+    [Test]
+    public void StaticEventTest()
+    {
+        WeakReference weakReference = null;
+
+        // Run this in a delegate so that the local variable gets garbage collected (if that's possible)
+        new Action(() =>
         {
-            WeakReference weakReference = null;
-
-            // Run this in a delegate so that the local variable gets garbage collected (if that's possible)
-            new Action(() =>
-            {
-                var sut = new ShortLiving();
-                sut.SubscribeToStaticEvent();
-                weakReference = new WeakReference(sut);
-            })();
+            var sut = new ShortLiving();
+            sut.SubscribeToStaticEvent();
+            weakReference = new WeakReference(sut);
+        })();
 
 
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-            GC.Collect();
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
 
-            weakReference.Target.ShouldNotBeNull();
+        weakReference.Target.ShouldNotBeNull();
+    }
+}
+
+internal class LongLivingWithWeakEvent
+{
+    private readonly WeakEventManager<bool> _weakActionEventManager = new();
+
+    public event Action<bool> ReadOnlyChanged
+    {
+        add => _weakActionEventManager.AddEventHandler(value);
+        remove => _weakActionEventManager.RemoveEventHandler(value);
+    }
+}
+
+
+internal class LongLivingWithStaticWeakEvent
+{
+    private static readonly WeakEventManager<bool> WeakActionEventManager = new();
+
+    public static event Action<bool> ReadOnlyChanged
+    {
+        add => WeakActionEventManager.AddEventHandler(value);
+        remove => WeakActionEventManager.RemoveEventHandler(value);
+    }
+}
+
+
+internal class LongLivingWithEvent
+{
+    public event Action<bool> ReadOnlyChanged;
+
+    public static event Action<bool> StaticReadOnlyChanged;
+
+    // ReSharper disable once UnusedMember.Global
+    protected virtual void OnReadOnlyChanged(bool obj) => ReadOnlyChanged?.Invoke(obj);
+    protected virtual void OnStaticReadOnlyChanged(bool obj) => StaticReadOnlyChanged?.Invoke(obj);
+}
+
+
+internal class ShortLiving
+{
+    // ReSharper disable once NotAccessedField.Local
+    private bool _isReadOnly;
+
+    public ShortLiving(LongLivingWithWeakEvent longLiving)
+    {
+        longLiving.ReadOnlyChanged += OnReadOnlyChanged;
+    }
+
+    public ShortLiving(LongLivingWithEvent longLiving)
+    {
+        longLiving.ReadOnlyChanged += OnReadOnlyChanged;
+    }
+
+    public ShortLiving()
+    {
+    }
+
+    ~ShortLiving()
+    {
+        if (Debugger.IsAttached)
+        {
+            Debugger.Break();
         }
     }
 
-    internal class LongLivingWithWeakEvent
-    {
-        private readonly WeakEventManager<bool> _weakActionEventManager = new WeakEventManager<bool>();
+    public void SubscribeToStaticWeakEvent() => LongLivingWithStaticWeakEvent.ReadOnlyChanged += OnReadOnlyChanged;
+    public void SubscribeToStaticEvent() => LongLivingWithEvent.StaticReadOnlyChanged += OnReadOnlyChanged;
 
-        public event Action<bool> ReadOnlyChanged
-        {
-            add => _weakActionEventManager.AddEventHandler(value);
-            remove => _weakActionEventManager.RemoveEventHandler(value);
-        }
-    }
-
-
-    internal class LongLivingWithStaticWeakEvent
-    {
-        private static readonly WeakEventManager<bool> WeakActionEventManager = new WeakEventManager<bool>();
-
-        public static event Action<bool> ReadOnlyChanged
-        {
-            add => WeakActionEventManager.AddEventHandler(value);
-            remove => WeakActionEventManager.RemoveEventHandler(value);
-        }
-    }
-
-
-    internal class LongLivingWithEvent
-    {
-        public event Action<bool> ReadOnlyChanged;
-
-        public static event Action<bool> StaticReadOnlyChanged;
-
-        // ReSharper disable once UnusedMember.Global
-        protected virtual void OnReadOnlyChanged(bool obj) => ReadOnlyChanged?.Invoke(obj);
-        protected virtual void OnStaticReadOnlyChanged(bool obj) => StaticReadOnlyChanged?.Invoke(obj);
-    }
-
-
-    internal class ShortLiving
-    {
-        // ReSharper disable once NotAccessedField.Local
-        private bool _isReadOnly;
-
-        public ShortLiving(LongLivingWithWeakEvent longLiving) => longLiving.ReadOnlyChanged += OnReadOnlyChanged;
-        public ShortLiving(LongLivingWithEvent longLiving) => longLiving.ReadOnlyChanged += OnReadOnlyChanged;
-
-        public ShortLiving()
-        {
-        }
-
-        public void SubscribeToStaticWeakEvent() => LongLivingWithStaticWeakEvent.ReadOnlyChanged += OnReadOnlyChanged;
-        public void SubscribeToStaticEvent() => LongLivingWithEvent.StaticReadOnlyChanged += OnReadOnlyChanged;
-
-        private void OnReadOnlyChanged(bool isReadOnly) => _isReadOnly = isReadOnly;
-    }
+    private void OnReadOnlyChanged(bool isReadOnly) => _isReadOnly = isReadOnly;
 }
--- WpfExamples/MainWindow.xaml.cs
+++ WpfExamples/MainWindow.xaml.cs
@@ -11,214 +11,209 @@ using System.Windows.Media;
 using System.Windows.Threading;
 using Microsoft.ConcurrencyVisualizer.Instrumentation;
 
-namespace WpfExamples
+namespace WpfExamples;
+
+public partial class MainWindow : INotifyPropertyChanged
 {
-    public partial class MainWindow : INotifyPropertyChanged
+    private const int ElementsToAddCount = 10;
+    private readonly int _delay = 50;
+    private readonly int _maxElements = 306;
+    private readonly Random _random = new();
+    private int _elementsDisplayed;
+    private int _elementsInDispatcherQueue;
+    private int _elementsPushedToDispatcher;
+
+    private bool _heavyLoadStarted;
+    private Color _previousColor;
+    private DispatcherPriority _selectedAddNewItemPriority = DispatcherPriority.Background;
+    private DispatcherPriority _selectedDispatcherPriority = DispatcherPriority.Background;
+
+    public MainWindow()
     {
-        private const int ElementsToAddCount = 10;
-        private readonly int _delay = 50;
-        private readonly int _maxElements = 306;
-        private readonly Random _random = new Random();
-        private int _elementsDisplayed;
-        private int _elementsInDispatcherQueue;
-        private int _elementsPushedToDispatcher;
+        Items = new ObservableCollection<Brush>();
+        var dispatcherPriorities = Enum.GetValues(typeof(DispatcherPriority)).Cast<DispatcherPriority>();
+        DispatcherPriorities = new ObservableCollection<DispatcherPriority>(dispatcherPriorities);
 
-        private bool _heavyLoadStarted;
-        private Color _previousColor;
-        private DispatcherPriority _selectedAddNewItemPriority = DispatcherPriority.Background;
-        private DispatcherPriority _selectedDispatcherPriority = DispatcherPriority.Background;
+        SelectedDispatcherPriority = DispatcherPriority.Background;
 
-        public MainWindow()
+        DataContext = this;
+
+        InitializeComponent();
+
+        Enumerable.Range(0, _maxElements).ToList().ForEach(_ => AddNewNormalItem());
+        _elementsPushedToDispatcher = 0;
+        _elementsDisplayed = 0;
+        ElementsInDispatcherQueue = 0;
+    }
+
+    public ObservableCollection<Brush> Items { get; }
+    public ObservableCollection<DispatcherPriority> DispatcherPriorities { get; }
+
+
+    public int ElementsInDispatcherQueue
+    {
+        get => _elementsInDispatcherQueue;
+        set
         {
-            Items = new ObservableCollection<Brush>();
-            var dispatcherPriorities = Enum.GetValues(typeof(DispatcherPriority)).Cast<DispatcherPriority>();
-            DispatcherPriorities = new ObservableCollection<DispatcherPriority>(dispatcherPriorities);
+            _elementsInDispatcherQueue = value;
+            OnPropertyChanged();
+        }
+    }
 
-            SelectedDispatcherPriority = DispatcherPriority.Background;
+    public DispatcherPriority SelectedDispatcherPriority
+    {
+        get => _selectedDispatcherPriority;
+        set
+        {
+            _selectedDispatcherPriority = value;
+            OnPropertyChanged();
+        }
+    }
 
-            DataContext = this;
+    public DispatcherPriority SelectedAddNewItemPriority
+    {
+        get => _selectedAddNewItemPriority;
+        set
+        {
+            _selectedAddNewItemPriority = value;
+            OnPropertyChanged();
+        }
+    }
 
-            InitializeComponent();
+    // TODO: Add switch for DispatcherPriority to show that application can be hanged
+    // TODO: and separate for special operations
 
-            Enumerable.Range(0, _maxElements).ToList().ForEach(_ => AddNewNormalItem());
-            _elementsPushedToDispatcher = 0;
-            _elementsDisplayed = 0;
-            ElementsInDispacherQueue = 0;
+    private async void OnCreateHeavyLoad(object sender, RoutedEventArgs e)
+    {
+        _heavyLoadStarted = true;
+
+        _previousColor = new Color();
+
+        while (_heavyLoadStarted)
+        {
+            Debug.WriteLine($"ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+
+            _elementsPushedToDispatcher++;
+
+            Application.Current?.Dispatcher.BeginInvoke(new Action(AddNewNormalItem), SelectedDispatcherPriority);
+
+            await Task.Delay(_delay - 5).ConfigureAwait(false);
+        }
+    }
+
+    private void AddNewNormalItem()
+    {
+        var newColor = new Color
+        {
+            A = 150,
+            R = GetNewColor(_previousColor.R),
+            G = GetNewColor(_previousColor.G),
+            B = GetNewColor(_previousColor.B)
+        };
+
+        _previousColor = newColor;
+
+        AddItem(newColor, _delay);
+    }
+
+    private byte GetNewColor(byte previousColor)
+    {
+        return (byte) Math.Abs(previousColor + _random.Next(50) - 25);
+    }
+
+    private void OnStopHeavyLoad(object sender, RoutedEventArgs e)
+    {
+        _heavyLoadStarted = false;
+    }
+
+    private void AddNewSpecialItem()
+    {
+        var newColor = new Color
+        {
+            A = 255,
+            R = 255,
+            G = 0,
+            B = 0
+        };
+
+        AddItem(newColor, _delay * 6);
+    }
+
+    private void AddItem(Color colour, int delay)
+    {
+        Items.Add(new SolidColorBrush(colour));
+
+        ElementsInDispatcherQueue = _elementsPushedToDispatcher - ++_elementsDisplayed;
+
+        if (Items.Count > _maxElements) Items.RemoveAt(0);
+
+        if (Items.Count == _maxElements)
+        {
+            var span = Markers.EnterSpan($"Starting delay: {delay}");
+            Thread.Sleep(delay);
+            // SpinWait.SpinUntil(() => false, delay);
+            // Thread.SpinWait(delay);
+            span.Leave();
+        }
+    }
+
+    private void OnAddWithInvoke(object sender, RoutedEventArgs e)
+    {
+        InvokeButton.IsEnabled = false;
+
+        for (var i = 0; i < ElementsToAddCount; i++)
+        {
+            _elementsPushedToDispatcher++;
+            Application.Current.Dispatcher.Invoke(AddNewSpecialItem, SelectedAddNewItemPriority);
         }
 
-        public ObservableCollection<Brush> Items { get; }
-        public ObservableCollection<DispatcherPriority> DispatcherPriorities { get; }
+        InvokeButton.IsEnabled = true;
+    }
 
+    private void OnAddWithBeginInvoke(object sender, RoutedEventArgs e)
+    {
+        BeginInvokeButton.IsEnabled = false;
 
-        public int ElementsInDispacherQueue
+        for (var i = 0; i < ElementsToAddCount; i++)
         {
-            get => _elementsInDispatcherQueue;
-            set
-            {
-                _elementsInDispatcherQueue = value;
-                OnPropertyChanged();
-            }
+            _elementsPushedToDispatcher++;
+            Application.Current.Dispatcher.BeginInvoke(new Action(AddNewSpecialItem), SelectedAddNewItemPriority);
         }
 
-        public DispatcherPriority SelectedDispatcherPriority
-        {
-            get => _selectedDispatcherPriority;
-            set
-            {
-                _selectedDispatcherPriority = value;
-                OnPropertyChanged();
-            }
-        }
+        BeginInvokeButton.IsEnabled = true;
+    }
 
-        public DispatcherPriority SelectedAddNewItemPriority
-        {
-            get => _selectedAddNewItemPriority;
-            set
-            {
-                _selectedAddNewItemPriority = value;
-                OnPropertyChanged();
-            }
-        }
+    private async void OnAddWithBeginInvokeAsync(object sender, RoutedEventArgs e)
+    {
+        InvokeAsyncButton.IsEnabled = false;
 
-        // TODO: Add switch for DispatcherPriority to show that application can be hanged
-        // TODO: and separate for special operations
+        _elementsPushedToDispatcher += ElementsToAddCount;
 
-        private async void OnCreateHeavyLoad(object sender, RoutedEventArgs e)
-        {
-            _heavyLoadStarted = true;
+        for (var i = 0; i < ElementsToAddCount; i++)
+            await Application.Current.Dispatcher.InvokeAsync(AddNewSpecialItem, SelectedAddNewItemPriority);
 
-            _previousColor = new Color();
+        InvokeAsyncButton.IsEnabled = true;
+    }
 
-            while (_heavyLoadStarted)
-            {
-                Debug.WriteLine($"ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+    private async void OnAddWithBeginInvokeAsyncAndWhenAll(object sender, RoutedEventArgs e)
+    {
+        WhenAllButton.IsEnabled = false;
 
-                _elementsPushedToDispatcher++;
+        _elementsPushedToDispatcher += ElementsToAddCount;
 
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                if (Application.Current != null)
-                    Application.Current.Dispatcher.BeginInvoke(new Action(AddNewNormalItem),
-                        SelectedDispatcherPriority);
-#pragma warning restore CS4014
+        var tasks = Enumerable.Range(0, ElementsToAddCount)
+            .Select(_ =>
+                Application.Current.Dispatcher.InvokeAsync(AddNewSpecialItem, SelectedAddNewItemPriority).Task);
 
-                await Task.Delay(_delay - 5).ConfigureAwait(false);
-            }
-        }
+        await Task.WhenAll(tasks);
 
-        private void AddNewNormalItem()
-        {
-            var newColor = new Color
-            {
-                A = 150,
-                R = GetNewColor(_previousColor.R),
-                G = GetNewColor(_previousColor.G),
-                B = GetNewColor(_previousColor.B)
-            };
+        WhenAllButton.IsEnabled = true;
+    }
 
-            _previousColor = newColor;
+    public event PropertyChangedEventHandler PropertyChanged;
 
-            AddItem(newColor, _delay);
-        }
-
-        private byte GetNewColor(byte previousColor)
-        {
-            return (byte) Math.Abs(previousColor + _random.Next(50) - 25);
-        }
-
-        private void OnStopHeavyLoad(object sender, RoutedEventArgs e)
-        {
-            _heavyLoadStarted = false;
-        }
-
-        private void AddNewSpecialItem()
-        {
-            var newColor = new Color
-            {
-                A = 255,
-                R = 255,
-                G = 0,
-                B = 0
-            };
-
-            AddItem(newColor, _delay * 6);
-        }
-
-        private void AddItem(Color colour, int delay)
-        {
-            Items.Add(new SolidColorBrush(colour));
-
-            ElementsInDispacherQueue = _elementsPushedToDispatcher - ++_elementsDisplayed;
-
-            if (Items.Count > _maxElements) Items.RemoveAt(0);
-
-            if (Items.Count == _maxElements)
-            {
-                var span = Markers.EnterSpan($"Starting delay: {delay}");
-                Thread.Sleep(delay);
-                // SpinWait.SpinUntil(() => false, delay);
-                // Thread.SpinWait(delay);
-                span.Leave();
-            }
-        }
-
-        private void OnAddWithInvoke(object sender, RoutedEventArgs e)
-        {
-            InvokeButton.IsEnabled = false;
-
-            for (var i = 0; i < ElementsToAddCount; i++)
-            {
-                _elementsPushedToDispatcher++;
-                Application.Current.Dispatcher.Invoke(AddNewSpecialItem, SelectedAddNewItemPriority);
-            }
-
-            InvokeButton.IsEnabled = true;
-        }
-
-        private void OnAddWithBeginInvoke(object sender, RoutedEventArgs e)
-        {
-            BeginInvokeButton.IsEnabled = false;
-
-            for (var i = 0; i < ElementsToAddCount; i++)
-            {
-                _elementsPushedToDispatcher++;
-                Application.Current.Dispatcher.BeginInvoke(new Action(AddNewSpecialItem), SelectedAddNewItemPriority);
-            }
-
-            BeginInvokeButton.IsEnabled = true;
-        }
-
-        private async void OnAddWithBeginInvokeAsync(object sender, RoutedEventArgs e)
-        {
-            InvokeAsyncButton.IsEnabled = false;
-
-            _elementsPushedToDispatcher += ElementsToAddCount;
-
-            for (var i = 0; i < ElementsToAddCount; i++)
-                await Application.Current.Dispatcher.InvokeAsync(AddNewSpecialItem, SelectedAddNewItemPriority);
-
-            InvokeAsyncButton.IsEnabled = true;
-        }
-
-        private async void OnAddWithBeginInvokeAsyncAndWhenAll(object sender, RoutedEventArgs e)
-        {
-            WhenAllButton.IsEnabled = false;
-
-            _elementsPushedToDispatcher += ElementsToAddCount;
-
-            var tasks = Enumerable.Range(0, ElementsToAddCount)
-                .Select(_ =>
-                    Application.Current.Dispatcher.InvokeAsync(AddNewSpecialItem, SelectedAddNewItemPriority).Task);
-
-            await Task.WhenAll(tasks);
-
-            WhenAllButton.IsEnabled = true;
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
